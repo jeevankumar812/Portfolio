8010049ac545a3b0051c7e0188f8997674d0719f
K Jeevan Kumar <jeevanjeevan63643@gmail.com>
2025-07-15 22:06:45 +0530

    Added Linked In and Github

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -17,9 +17,8 @@ const Footer = ({isDarkMode}) => {
     <div className='text-center sm:flex items-center justify-between border-t border-gray-400 mx-[10%] mt-12 py-6'>
         <p>© 2025 GreatStack. All rights reserved.</p>
         <ul className='flex items-center gap-10 justify-center mt-4 sm:mt-0'>
-            <li><a target='_blank' href="https://instagram.com/greatstackdev">GitHub</a></li>
-            <li><a target='_blank' href="https://instagram.com/greatstackdev">LinkedIn</a></li>
-            <li><a target='_blank' href="https://instagram.com/greatstackdev">Twitter</a></li>
+            <li><a target='_blank' href="https://github.com/jeevankumar812">GitHub</a></li>
+            <li><a target='_blank' href="https://www.linkedin.com/in/k-jeevan-kumar-5b540b266/">LinkedIn</a></li>
         </ul>
     </div>
 
